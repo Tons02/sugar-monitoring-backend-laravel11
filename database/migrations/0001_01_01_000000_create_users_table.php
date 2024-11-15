1a89a9f91f935969f7b98725c9a1d40dc4d9a151
Antonio Jr. D. Montilla <system.montilla@gmail.com>
2024-11-15 23:32:48 +0800

    create user and daily sugar migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,12 +13,18 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name');
+            $table->string('last_name');
+            $table->enum("gender", ["male", "female"]);
+            $table->string('mobile_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string(column: 'username')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
---
+++ database/migrations/2024_11_15_152621_create_daily_sugar_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('daily_sugar', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger(column: "user_id")->index();
+            $table->double('mgdl');
+            $table->string('description');
+            $table->dateTime('date');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign("user_id")
+            ->references("id")
+            ->on("users")
+            ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('daily_sugar');
+    }
+};
